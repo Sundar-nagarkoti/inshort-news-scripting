3b586e375774753e626cbebd52047784448f0587
Sundar nagarkoti <38424101+Sundar-nagarkoti@users.noreply.github.com>
2021-05-09 13:46:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,42 +4,43 @@ const fs=require("fs");
 
 request("https://inshorts.com/en/read/",callback);
 
-let headingarr = [];
-let summery = [];
-let newlinks = [];
-let mynews = [];
+let HeadingArr = [];
+let Summery = [];
+let NewsLinks = [];
+let MyNews = [];
 function callback(err,res,html){
     if(!err){
         let $=cheerio.load(html);
-        let newsheading = $("a.clickable");
-        for(let i = 0 ; i < newsheading.length;i++){
-            let heading = $(newsheading[i]).text();
-            let newslinks = $(newsheading[i]).attr("href");
-            let mylinks = "https://inshorts.com"+newslinks;
-            newlinks.push(mylinks);
-            let hd = heading.trim();
-            headingarr.push(hd);
+        let NewsHeading = $("a.clickable");
+        for(let i = 0 ; i < NewsHeading.length;i++){
+            let Heading = $(NewsHeading[i]).text();
+            let Links = $(NewsHeading[i]).attr("href");
+            let MyLinks = "https://inshorts.com"+Links;
+            NewsLinks.push(MyLinks);
+            let hd = Heading.trim();
+            HeadingArr.push(hd);
         }
 
         let des = $(".news-card-content.news-right-box div");
         for(let i = 0 ; i < des.length;i = i+2){
             let newsdes = $(des[i]).text();
-            summery.push(newsdes);
+            Summery.push(newsdes);
         }
 
 
 
         for(let i = 0; i < 25; i++){
-            mynews.push({
-                "NEWSHEADING" : headingarr[i],
-                "NEWSUMMERY" : summery[i],
-                "NEWSLINKS" : newlinks[i]
+            MyNews.push({
+                "NEWSN0" : i+1,
+                "NEWSHEADING" : HeadingArr[i],
+                "NEWSUMMERY" : Summery[i],
+                "NEWSLINKS" : NewsLinks[i]
             })
         }
         for(let i=0;i<25;i++){
-            console.log(mynews[i]);
+            console.log(MyNews[i]);
         }
         
-        fs.writeFileSync("mynews.json",JSON.stringify(mynews));
+        fs.writeFileSync("mynews.json",JSON.stringify(MyNews));
     }
 }
